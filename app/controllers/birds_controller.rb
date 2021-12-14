bbfc91e9cf8821a95fa0d23085977aa66050862e
Ben Jackson <benjacksonSE@gmail.com>
2021-12-13 20:31:36 -0500

    commit tree e76648fb02f6f44fb47324975aa53dc3a63f9d86

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,8 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  validates :name, presence: true, uniqueness: true
+
   # GET /birds
   def index
     birds = Bird.all
@@ -10,7 +12,11 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    if bird.valid?
+      render json: bird, status: :created
+    else
+      render jason: { errors: bird.errors }, status: :unprocessable_entity
+    end
   end
 
   # GET /birds/:id
